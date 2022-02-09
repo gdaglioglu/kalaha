c71a1bb6a5250c438a003743cc10c9d4d750ad5d
Gokhan Daglioglu <gokhandaglioglu@gokhans-mbp.home>
2022-02-09 11:08:20 +0100

    adding javadocs to a method that I missed

--- src/main/java/com/kalaha/application/GameApplication.java
+++ src/main/java/com/kalaha/application/GameApplication.java
@@ -3,6 +3,7 @@ package com.kalaha.application;
 import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -12,11 +13,18 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class GameApplication {
 
-    public static void main(String[] args) {SpringApplication.run(GameApplication.class, args);}
+    public static void main(String[] args) {
+        SpringApplication.run(GameApplication.class, args);
+    }
 
+    /**
+     * Creates and returns a rest template bean.
+     * @param builder the rest template builder.
+     * @return the rest template.
+     */
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 }
